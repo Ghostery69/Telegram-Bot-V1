763b2462f53f6834927fc6671039adc6c9244709
Ghostery69 <157644164+Ghostery69@users.noreply.github.com>
2025-01-12 21:25:17 +0000

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -2,7 +2,7 @@ module.exports = (bot) => {
   bot.command('start', (ctx) => {
     const userName = ctx.from.first_name; 
 
-    const welcomeMessage = `👋 Bonjour ${userName} \n\nBienvenue chère utilisateur ou utilisatrice moi c'est Spirity Chatbot \n\n- /help consultez la liste de mes commandes \n\n veuillez contacter @Spirityh4ck si le bot à un problème 🤗`;
+    const welcomeMessage = `👋 𝗕𝗼𝗻𝗷𝗼𝘂𝗿𝘀 ${userName} \n\n𝗕𝗶𝗲𝗻𝘃𝗲𝗻𝘂𝗲 𝗰𝗵𝗲𝗿𝘀 𝘂𝘁𝗶𝗹𝗶𝘀𝗮𝘁𝗲𝘂𝗿𝘀 𝗼𝘂 𝘂𝘁𝗶𝗹𝗶𝘀𝗮𝘁𝗿𝗶𝗰𝗲𝘀 𝗰'𝗲𝘀𝘁 𝗺𝗼𝗶 𝘀𝗽𝗶𝗿𝗶𝘁𝘆 𝗯𝗼𝘁 \n\n- /help consultez la liste de mes commandes \n\n veuillez contacter @Spirityh4ck si le bot à un problème 🤗`;
 
     ctx.reply(welcomeMessage);
   });
